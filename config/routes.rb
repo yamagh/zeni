7f8b5a980f1b6c9e6e0a8240e30e5f0fb15216bd
yamagh <hiroyuki.yamagami@gmail.com>
2017-04-08 15:15:57 +0900

    Add routing to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :logs
   resources :stores
-  resources :sub_categories
-  resources :categories
+  resources :categories do
+    resources :sub_categories
+  end
   resources :accounts
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'logs#index'
 end

954fe026b1be764c597b63bc71b59bebe7cf42b3
yamagh <hiroyuki.yamagami@gmail.com>
2017-04-22 12:08:44 +0900

    Add timezone settings

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Zeni
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.template_engine :slim
     end

e3bccb9cb3a9fe55a348a4b125f3175b879d124e
yamagh <hiroyuki.yamagami@gmail.com>
2017-05-01 15:32:38 +0900

    Change data type to string at users.tel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429142555) do
+ActiveRecord::Schema.define(version: 20170501062442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20170429142555) do
   create_table "stores", force: :cascade do |t|
     t.integer  "user_id",                     null: false
     t.string   "name",                        null: false
-    t.integer  "tel"
+    t.string   "tel"
     t.string   "address"
     t.boolean  "is_disabled", default: false, null: false
     t.integer  "order",       default: 0,     null: false

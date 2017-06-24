61aad24656d84e1503dfe7cfae6f0ee6cab4e745
yamagh <hiroyuki.yamagami@gmail.com>
2017-06-24 16:48:26 +0900

    Add tests of Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :sub_categories, dependent: :destroy
+
+  validates :name, length: { minimum: 1 }
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @bob = users(:bob)
+  end
+
+  test "can create new category" do
+    assert_difference ('Category.count') do
+      Category.create user_id: @bob.id, name: "foods"
+    end
+  end
+
+  test "default value of order is 0" do
+    foods = Category.create(user_id: @bob.id, name: "foods")
+    assert_equal( foods.order, 0 ) 
+  end
+
+  test "default value of is_disabled is false" do
+    foods = Category.create(user_id: @bob.id, name: "foods")
+    assert_equal( foods.is_disabled, false ) 
+  end
+
+  test "should have name" do
+    assert_no_difference ('Category.count') do
+      Category.create user_id: @bob.id
+    end
+  end
+
+  test "should have 1 or more length name" do
+    assert_no_difference ('Category.count') do
+      Category.create user_id: @bob.id, name: ""
+    end
+  end
 end

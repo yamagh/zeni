26f2aef3dc5f0b8b53c612e5ada4ca4796fa15c8
yamagh <hiroyuki.yamagami@gmail.com>
2017-06-24 20:26:02 +0900

    Add tests of SubCategory

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,4 +1,6 @@
 class SubCategory < ApplicationRecord
   belongs_to :category
   has_many :logs, dependent: :destroy
+
+  validates :name, length: { minimum: 1, maximum: 50 }
 end
--- test/models/sub_category_test.rb
+++ test/models/sub_category_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class SubCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @bob = users(:bob)
+    @food = categories(:food)
+  end
+
+  test "can create" do
+    assert_difference ('SubCategory.count') do
+      SubCategory.create category_id: @food.id, name: "Drink"
+    end
+  end
+
+  test "should have name" do
+    assert_no_difference ('SubCategory.count') do
+      SubCategory.create category_id: @food.id
+    end
+  end
+
+  test "should be 50 or less length name" do
+    assert_difference ('SubCategory.count') do
+      SubCategory.create category_id: @food.id, name: [*1..50].map{"a"}*''
+    end
+
+    assert_no_difference ('SubCategory.count') do
+      SubCategory.create category_id: @food.id, name: [*1..51].map{"a"}*''
+    end
+  end
 end

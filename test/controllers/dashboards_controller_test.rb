a983744baf9cf47b51cdb0ddf92447329cc363e5
yamagh <hiroyuki.yamagami@gmail.com>
2017-06-25 18:31:37 +0900

    Add setup to dashbords_controller test

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    Warden.test_mode!
+    @user = users( :bob )
+    login_as(@user, :scope => :user)
+  end
+
   test "should get show" do
     get dashboards_show_url
     assert_response :success
   end
-
 end

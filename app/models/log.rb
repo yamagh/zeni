be8afc2d1f90e6b51a572bab1c6695f062e6a786
yamagh <hiroyuki.yamagami@gmail.com>
2017-11-11 16:22:36 +0900

    Add validates to Log.amount

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,4 +3,7 @@ class Log < ApplicationRecord
   belongs_to :account
   belongs_to :sub_category
   belongs_to :store
+
+  validates :ammount, numericality: { greater_than_or_equal_to: 0,
+                                      less_than: 1_000_000_000_000_000 }
 end
--- test/models/log_test.rb
+++ test/models/log_test.rb
@@ -29,4 +29,38 @@ class LogTest < ActiveSupport::TestCase
   test "`is_expence` default value is true" do
     assert_equal( true, @coffee.is_expence )
   end
+
+  test "ammount should be decimal" do
+    assert_no_difference 'Log.count' do
+      Log.create user_id:         @bob.id,
+                 account_id:      @coffee.account_id,
+                 sub_category_id: @coffee.sub_category_id,
+                 item:            "Tea",
+                 ammount:         "1,000"
+    end
+  end
+
+  test "amount should be greater than or equal to 0" do
+    @coffee.ammount = -1
+    @coffee.save
+    assert_not_equal -1, Log.find(@coffee.id).ammount
+
+    assert_no_difference 'Log.count' do
+      Log.create user_id: @bob.id,
+                 account_id: @coffee.account_id,
+                 sub_category_id: @coffee.sub_category_id,
+                 item: "Tea",
+                 ammount: -1
+    end
+  end
+
+  test "amount should be less than 1,000,000,000,000,000" do
+    @coffee.ammount = 999_999_999_999_999
+    @coffee.save
+    assert_equal 999_999_999_999_999, Log.find(@coffee.id).ammount
+
+    @coffee.ammount = 1_000_000_000_000_000
+    @coffee.save
+    assert_not_equal 1_000_000_000_000_000, Log.find(@coffee.id).ammount
+  end
 end
